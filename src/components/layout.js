cc6b18b09c39f5d94e1cce523ed5d60eeb25663d
Henry <henry_ball@hotmail.co.uk>
2019-05-30 17:40:13 +0100

    pass logo props to header

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,17 +14,19 @@ import Header from "./header"
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+      {
+        airtable(table: {eq: "Config"}) {
+          data {
+            Logo {
+              url
+            }
           }
         }
       }
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <Header logo={data.airtable.data.Logo[0].url} />
         <div>
           <main className='main'>
             <div className='content-wrapper'>
