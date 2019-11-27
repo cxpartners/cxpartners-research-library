4bdcf19de08504f9b5763213e91f8f073c3383fd
Henry <henry_ball@hotmail.co.uk>
2019-11-27 10:50:05 +0000

    Update outdated packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,96 +1,82 @@
 require('dotenv').config({
   path: '.env',
-})
-
-const autoprefixer = require('autoprefixer');
+});
 
 module.exports = {
   siteMetadata: {
-    title: `cxpartners' Research Library`,
-    description: `This example library is a demonstration of a central resource containing customer insights and hypotheses, as well as details of the research activities that informed them.`,
-    author: `cxpartners`,
+    title: 'cxpartners Research Library',
+    description: 'This example library is a demonstration of a central resource containing customer insights and hypotheses, as well as details of the research activities that informed them.',
+    author: 'cxpartners',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-eslint',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sass',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
     {
-      resolve: `gatsby-plugin-postcss`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        postCssPlugins: [
-          autoprefixer({
-            browsers: ['last 2 versions', 'not ie 10'],
-            grid: true,
-          })
-        ],
-        precision: 8
-      }
-    },
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Research Library`,
-        short_name: `Research Library`,
-        start_url: `/`,
-        background_color: `#0e8add`,
-        theme_color: `#0e8add`,
-        display: `minimal-ui`,
-        icon: `src/images/cxpartners-logo.png`,
+        name: 'Research Library',
+        short_name: 'Research Library',
+        start_url: '/',
+        background_color: '#0e8add',
+        theme_color: '#0e8add',
+        display: 'minimal-ui',
+        icon: 'src/images/cxpartners-logo.png',
       },
     },
-    `gatsby-plugin-sass`,
     {
-      resolve: `gatsby-source-airtable`,
+      resolve: 'gatsby-source-airtable',
       options: {
         apiKey: process.env.AIRTABLE_API_KEY,
         tables: [
           {
             baseId: process.env.AIRTABLE_BASE,
-            tableName: `Opportunities`,
-            tableLinks: [`Personas`, `Example_Concepts`, `Key_Insights`, `Supporting_Insights`]
+            tableName: 'Opportunities',
+            tableLinks: ['Personas', 'Example_Concepts', 'Key_Insights', 'Supporting_Insights'],
           },
           {
             baseId: process.env.AIRTABLE_BASE,
-            tableName: `Insights`,
+            tableName: 'Insights',
           },
           {
             baseId: process.env.AIRTABLE_BASE,
-            tableName: `Stories`,
+            tableName: 'Stories',
           },
           {
             baseId: process.env.AIRTABLE_BASE,
-            tableName: `Concepts`,
+            tableName: 'Concepts',
           },
           {
             baseId: process.env.AIRTABLE_BASE,
-            tableName: `Personas`,
+            tableName: 'Personas',
           },
           {
             baseId: process.env.AIRTABLE_BASE,
-            tableName: `Activities`,
+            tableName: 'Activities',
           },
           {
             baseId: process.env.AIRTABLE_BASE,
-            tableName: `Studies`,
+            tableName: 'Studies',
           },
           {
             baseId: process.env.AIRTABLE_BASE,
-            tableName: `Modes`,
+            tableName: 'Modes',
           },
           {
             baseId: process.env.AIRTABLE_BASE,
-            tableName: `Config`,
-          }
-        ]
-      }
-    }
-    // `gatsby-plugin-offline`,
+            tableName: 'Config',
+          },
+        ],
+      },
+    },
   ],
-}
+};
