8c4c8bc0d97d9b97650fe6e26a5633cbd2089870
Henry <henry_ball@hotmail.co.uk>
2019-12-06 10:56:20 +0000

    Add hero divider

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,40 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Badges from './badges';
+import randomColour from '../utils/randomColour';
 
 const Hero = ({
   imgUrl,
+  backgroundColour,
   recordId,
   location,
   label,
   title,
   attributes,
   personas,
-}) => (
-  <header>
-    { imgUrl && <div className="hero-image" style={{ backgroundImage: `url(${imgUrl})` }} /> }
-    <div className="container">
-      <div className="options">
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={recordId && `https://airtable.com/tblNJ0JhIFJEji8Hh/viws2I7nuQKNkZYHb/${recordId}`}
-          className="external"
-        >
-          View in Airtable
-        </a>
+}) => {
+  const colour = backgroundColour || randomColour();
+  return (
+    <header>
+      { imgUrl && <div className="hero-image" style={{ backgroundImage: `url(${imgUrl})` }} /> }
+      { !imgUrl && <div className="hero-divider" style={{ backgroundColor: colour }} />}
+      <div className="container">
+        <div className="options">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={recordId && `https://airtable.com/tblNJ0JhIFJEji8Hh/viws2I7nuQKNkZYHb/${recordId}`}
+            className="external"
+          >
+            View in Airtable
+          </a>
+        </div>
+        <h2 className="subtitle">{location}</h2>
+        <h3 className="headline">{ label ? `${label}: ${title}` : title}</h3>
+        <Badges
+          attributes={attributes || []}
+          personas={personas || []}
+        />
       </div>
-      <h2 className="subtitle">{location}</h2>
-      <h3 className="headline">{ label ? `${label}: ${title}` : title}</h3>
-      <Badges
-        attributes={attributes || []}
-        personas={personas || []}
-      />
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 Hero.defaultProps = {
+  backgroundColour: '',
   label: '',
   attributes: [],
   personas: [],
@@ -43,6 +50,7 @@ Hero.defaultProps = {
 
 Hero.propTypes = {
   imgUrl: PropTypes.string.isRequired,
+  backgroundColour: PropTypes.string,
   recordId: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   label: PropTypes.string,
